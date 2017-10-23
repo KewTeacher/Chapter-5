c58b267cc23085c90563bf158860b5a6cdb5fe3a
justishawkins <32522747+justishawkins@users.noreply.github.com>
2017-10-23 08:02:44 -0400

    in class practice

---
+++ practice1.py
@@ -0,0 +1,18 @@
+import math
+x = 35
+y = 35
+if x < y:
+    print("x is less than y")
+    
+elif x > y:
+    print("x is greater than y")
+elif x == y:
+    print("they are equal")
+   
+else:
+    print("x and y are equal")
+    
+    
+if x + y == 70:
+    print(" the sum of both is 70") 
+
---
+++ practice2.py
@@ -0,0 +1,4 @@
+w1 ="All"
+w2 = " work"
+print(w1 + w2)
+print(w1,w2)
